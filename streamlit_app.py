3f7c726737f3868da2e6c87c6bf2b3c070d6f737
hein.van.wijlen@vionfood.com <hein.van.wijlen@vionfood.com>
2023-03-15 15:30:09 +0100

    ab

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,17 +48,20 @@ streamlit.dataframe(fruits_to_show)
 
 #streamlit json/api call
 
+
+# streamlit api call section
 streamlit.header("Fruityvice Fruit Advice!")
 
-
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-
-# normalizes the json into a dataframe
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays the dataframe on the webpage
-streamlit.dataframe(fruityvice_normalized)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+        streamlit.error("please select a fruit for info")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    # normalizes the json into a dataframe
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # displays the dataframe on the webpage
+        streamlit.dataframe(fruityvice_normalized)
+     
+except URLError as e:
+    streamlit.error()
