a3abd45736ca278881813e04ec81bd4e902acdc0
hein.van.wijlen@vionfood.com <hein.van.wijlen@vionfood.com>
2023-03-15 15:49:41 +0100

    abc

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ if streamlit.button('get load list'):
 #function to allow user to add fruit to list
 def insert_row_snowflake(new_fruit):
      with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute("insert into fruit_load_list values ('"+ new_fruit +"')")
         return "thanks for adding " +new_fruit
 
 #input for fruit to add
