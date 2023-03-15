606fbf3acc46c8f7d5e2daecf022232cdce431b8
hein.van.wijlen@vionfood.com <hein.van.wijlen@vionfood.com>
2023-03-15 15:35:34 +0100

    ab

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ def get_fruityvice_data(this_fruit_choice):
 #display results  
 streamlit.header("Fruityvice Fruit Advice!")  
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("please select a fruit for info")
     else:
