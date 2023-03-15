93a773cd5cc89f143e0fbd2eab49d83627ef3abf
hein.van.wijlen@vionfood.com <hein.van.wijlen@vionfood.com>
2023-03-15 13:45:15 +0100

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,13 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# normalizes the json into a dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# displays the dataframe on the webpage
+#streamlit.dataframe(fruityvice_normalized)
+
+
+
 
 streamlit.title('My parents Healthy Diner')
 
