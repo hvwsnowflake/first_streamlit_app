dab3f0a645f74f21e62796eb0183a5b4c6952a7a
hein.van.wijlen@vionfood.com <hein.van.wijlen@vionfood.com>
2023-03-15 13:42:21 +0100

    abc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit
 import pandas
+import requests
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
 streamlit.title('My parents Healthy Diner')
